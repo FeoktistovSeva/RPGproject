b0954b43a614180320bd0003ae84aca7fc45d196
SevaF <Feoktistovseva@gmail.com>
2024-06-01 21:33:50 +0300

    fix: fix Game Menu

--- RPG/main.cpp
+++ RPG/main.cpp
@@ -5,15 +5,26 @@
 #include <cstring>
 #include <cstdlib>
 
+
 using namespace std;
 
 int main()
 {
 	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
+	srand(time(NULL));
 
 	int startG;
 	int slojnost;
+
+	int PlayerLVL = 0;
+
+	char Plname;
+	bool a = false;
+
+
+
+
 
 	cout << "\tнапишите 1 чтобы начать";
 	cout << endl;
@@ -47,13 +58,29 @@ int main()
 		cout << endl << endl;
 		cout << "Ввод: ";
 		cin >> slojnost;
+		system("cls");
+		a = true;
 	}
-	else
+
+
+
+	if (startG == 1)
 	{
-		cout << "НАЧАЛО ИГРЫ";
+		a = true;
 	}
 
 
+
+	if(a = true)
+	{
+		cout << "\tкак вас называть: ";
+		cin >> Plname;
+	}
+	system("cls");
+
+
+
+	cout << "negr";
 
 
 
