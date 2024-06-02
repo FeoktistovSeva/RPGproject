52163f35e558007a81899a92113f2f34d2741ba2
SevaF <Feoktistovseva@gmail.com>
2024-06-02 17:40:00 +0300

    feat: LVL & HP, Fix: game menu

--- RPG/main.cpp
+++ RPG/main.cpp
@@ -14,13 +14,37 @@ int main()
 	SetConsoleCP(1251);
 	srand(time(NULL));
 
+	
+	//переменные связанные с меню игры
 	int startG;
 	int slojnost;
+    bool a = false;
 
+	//переменные связанные с игроком
+    const int nameSIZE = 100;
 	int PlayerLVL = 0;
+	int PlayerHP = 100;
+	char Plname[nameSIZE];
+	
+	//переменные связанные с врагами
+	int EnemyLVL;
+	int enemyHP;
 
-	char Plname;
-	bool a = false;
+
+
+	if (slojnost == 1)
+	{
+		EnemyLVL = (PlayerLVL - 1) + rand() % ((PlayerLVL + 1) - (PlayerLVL - 1) + 1);
+	}
+	else if (slojnost == 2)
+	{
+		EnemyLVL = (PlayerLVL) + rand() % ((PlayerLVL + 2) - (PlayerLVL) + 1);
+	}
+	else
+	{
+		EnemyLVL = (PlayerLVL + 1) + rand() % ((PlayerLVL + 4) - (PlayerLVL + 1) + 1);
+	}
+
 
 
 
@@ -80,15 +104,48 @@ int main()
 
 
 
-	cout << "negr";
+	cout << "\tдобро пожаловать, " << Plname << "!";
+	Sleep(1700);
+	system("cls");
+
+	
+
+	cout << "\tо ";
+	Sleep(100);
+	cout << 'н';
+	Sleep(100);
+	cout << 'е';
+	Sleep(100);
+	cout << "т ";
+	Sleep(500);
+	cout << 'н';
+	Sleep(100);
+	cout << "а ";
+	Sleep(100);
+	cout << 'в';
+	Sleep(100);
+	cout << 'а';
+	Sleep(100);
+	cout << "с ";
+	Sleep(100);
+	cout << 'н';
+	Sleep(100);
+	cout << 'а';
+	Sleep(100);
+	cout << 'п';
+	Sleep(100);
+	cout << 'а';
+	Sleep(100);
+	cout << 'л';
+	Sleep(100);
+	cout << 'и';
+	
 
 
+	Sleep(1300);
+	system("cls");
 
-
-
-
-
-
+	
 
 
 
