7f22b1971a9fd78d0c77b6587eaf0d446c4aa4eb
SevaF <Feoktistovseva@gmail.com>
2024-06-16 20:45:15 +0300

    fix: balance

--- RPG/main.cpp
+++ RPG/main.cpp
@@ -18,31 +18,32 @@ int main()
 	SetConsoleCP(1251);
 	srand(time(NULL));
 
-	
+
 	//меню игры
 	int startG;
 	int slojnost = 1;
-    bool a = false;
+	bool a = false;
 
 	//игрок
-    const int nameSIZE = 100;
+	const int nameSIZE = 100;
 	float PlayerLVL = 1;
 	int PlayerHP = PlayerLVL * 10;
 	char Plname[nameSIZE];
 	int AtakOrHP = 0;
 	int PlayerAttak;
 	bool nextUroven = false;
-	
+
 	//враг
 	float EnemyLVL = 1;
 	unsigned enemyHP = 0;
 	int enemyChange;
+	unsigned enemyAttak = 0;
 
 
 	//инвентарь
-	int miniHeal = 0;
-	int BigHeal = 0;
-	int AttakBust = 0;
+	int miniHeal = 2;
+	int BigHeal = 1;
+	int AttakBust = 1;
 
 	int RandItem = 0;
 
@@ -59,7 +60,7 @@ int main()
 	cin >> startG;
 
 	system("cls");
-	
+
 
 
 	if (startG == 3)
@@ -96,7 +97,7 @@ int main()
 
 
 
-	if(a = true)
+	if (a = true)
 	{
 		cout << "\tвведите имя: ";
 		cin >> Plname;
@@ -105,11 +106,11 @@ int main()
 
 
 
-	cout << "\tдлбро пожаловать, " << Plname << "!";
+	cout << "\tдобро пожаловать, " << Plname << "!";
 	Sleep(1700);
 	system("cls");
 
-	
+
 
 	cout << "\tна вас напали!";
 
@@ -129,86 +130,107 @@ int main()
 	while (true)
 	{
 		MAXenemyHP = HPvrag(enemyHP, EnemyLVL);
-		for (;enemyHP > PlayerAttak;)
+		for (; enemyHP > PlayerAttak;)
 		{
-	      system("cls");
-	      enemyHP = FightGO(AtakOrHP, enemyHP, PlayerLVL, PlayerAttak);
-		  
-		
-
-  
-	      fight_menu_cout(enemyHP, Plname, PlayerHP, AtakOrHP);
-
-	
-		  cout << endl << "\t\tваш уровень: " << PlayerLVL << "\t     уровень врага: " << EnemyLVL;
-	      cout << endl << "\t\t\t       Ввод: ";
-
-		  
-
-	      cin >> AtakOrHP;
-
-		  RandItem = rand() % 10;
-
-		  if (AtakOrHP == 2)
-		  {
-			 int change = InventoryPrint(miniHeal, BigHeal, AttakBust, AtakOrHP);
+			enemyAttak++;
+			
+			
 
 
-			 if (change == 1 && miniHeal != 0)
-			 {
-				 PlayerHP += 5;
-				 miniHeal--;
-			 }
-			 else if (change == 2 && BigHeal != 0)
-			 {
-				 PlayerHP += 15;
-				 BigHeal--;
-			 }
-			 else if (change == 3 && AttakBust != 0)
-			 {
-				 if (change == 3 && AttakBust != 0)
-				 {
-					 AttakBust--;
-					 PlayerAttak += 3;
-				 }
-			 }
-			 else if (change == 4)
-			 {
-				 AtakOrHP = 0;
-			 }
-			 else
-			 {
-				 system("cls");
-				 cout << "\tошибка!";
-				 Sleep(500);
-				 AtakOrHP = 0;
-			 }
-
-		  }
 
 
-		  enemyChange = 1 + rand() % ( 2 - 1 ) - 1;
-		  system("cls");
-		  cout << "\tход противника...";
-		  Sleep(500);
-		 
+			system("cls");
+			enemyHP = FightGO(AtakOrHP, enemyHP, PlayerLVL, PlayerAttak);
 
-		  if (enemyChange == 2 && enemyHP != MAXenemyHP)
-		  {
-			  cout << "\n\tВраг лечится";
-			  enemyHP += 5;
-			  Sleep(700);
-		  }
-		  else
-		  {
-			  cout << "\n\tВраг атакует";
-			  PlayerHP -= EnemyLVL / 2;
-			  Sleep(700);
-		  }
+
+
+
+			fight_menu_cout(enemyHP, Plname, PlayerHP, AtakOrHP);
+
+
+			cout << endl << "\t\tваш уровень: " << PlayerLVL << "\t     уровень врага: " << EnemyLVL;
+			cout << endl << "\t\t\t       Ввод: ";
+
+
+
+			cin >> AtakOrHP;
+
+			RandItem = rand() % 10;
+
+			if (AtakOrHP == 2)
+			{
+				int change = InventoryPrint(miniHeal, BigHeal, AttakBust, AtakOrHP);
+
+
+				if (change == 1 && miniHeal != 0)
+				{
+					PlayerHP += 5;
+					miniHeal--;
+				}
+				else if (change == 2 && BigHeal != 0)
+				{
+					PlayerHP += 15;
+					BigHeal--;
+				}
+				else if (change == 3 && AttakBust != 0)
+				{
+					if (change == 3 && AttakBust != 0)
+					{
+						AttakBust--;
+						PlayerAttak += 3;
+					}
+				}
+				else if (change == 4)
+				{
+					AtakOrHP = 0;
+				}
+				else
+				{
+					system("cls");
+					cout << "\tошибка!";
+					Sleep(500);
+					AtakOrHP = 0;
+				}
+
+			}
+
+
+			if (enemyAttak > 0)
+			{
+				enemyChange = rand() % 10;
+				system("cls");
+				cout << "\tход противника...";
+				Sleep(500);
+
+
+				if (enemyChange > 5 && enemyHP < MAXenemyHP - 5)
+				{
+					cout << "\n\tВраг лечится";
+					enemyHP += 5;
+					Sleep(700);
+				}
+				else
+				{
+					cout << "\n\tВраг атакует";
+					PlayerHP -= EnemyLVL / 2;
+					Sleep(700);
+
+					if (PlayerHP == 0)
+					{
+						system("cls");
+						cout << "\n\t\t" << Plname << " погиб!";
+						exit(0);
+					}
+
+				}
+				enemyAttak--;
+			}
+
+			
 
 
 		}
-		
+
 
 		system("cls");
 		PlayerLVL += EnemyLVL / 5;
@@ -225,7 +247,7 @@ int main()
 
 
 		Sleep(700);
-		
+
 
 
 
@@ -255,11 +277,11 @@ int main()
 	}
 
 
-	MAXenemyHP = HPvrag(enemyHP, EnemyLVL);
 
-	
+
+
 
 
 	return 0;
-}
 
+}
